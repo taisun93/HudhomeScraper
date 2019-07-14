e40ed75bc5116d27fdf4e2e76feff58c4b712eab
David <taisun93@gmail.com>
2019-07-13 21:43:23 -0500

    Ran First Crawl

---
+++ HudhomeScraper/HudhomeScraper/spiders/HudhomestoreSpider.py
@@ -0,0 +1,23 @@
+import scrapy
+
+class HudhomestoreSpider(scrapy.Spider):
+    name = 'Hudhomestore'
+
+    def start_requests(self):
+        url = """https://www.hudhomestore.com/Listing/PropertySearchResult.aspx?pageId=1&sPageSize=1000&
+        zipCode=&city=&county=&sState=TX&fromPrice=0&toPrice=0&fCaseNumber=&bed=0&bath=0&street=&buyerType=0&
+        specialProgram=&Status=0&indoorAmenities=&outdoorAmenities=&housingType=&stories=&parking=&propertyAge=&
+        OrderbyName=SCASENUMBER&OrderbyValue=ASC&sLanguage=ENGLISH"""
+        
+        yield scrapy.Request(url=url, callback=self.parse)
+    
+    def parse(self, response):
+        propertylistings = response.xpath("//tr[contains(@class,'FormTableRow')]").getall()
+        print(len(propertylistings))
+        
+        print(propertylistings[2].xpath("(//u/text())[1]").get())
+
+
+        #for house in propertylistings:
+        #    print(house.get())
+        #    print(house.xpath("//u/text()").get())
